efa5c3a78bec7b6b68d7dea076d9e4492f57503c
Josh Massa <joshmassa@gmail.com>
2024-02-22 15:07:38 -0500

    added questions to index.js as well as functionality to generate a text file using the user's input

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Please enter a description for your project: '
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Please enter installation instructions for your project: '
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Please enter usage information: '
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Please enter your contribution guidelines: '
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Please enter any instructions for testing: '
+    }
+]).then((answers) => {
+    fs.writeFile('temp.doc', JSON.stringify(answers, null, '\t'), (err) => {
+        err ? console.error(err) : console.log('temp.doc Generated!')
+    });
+    console.log('Your answers were: ', answers);
+});
 
 // TODO: Create a function to initialize app
 function init() {}
